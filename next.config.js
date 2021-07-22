a1e02dfa1a55acb5de170171070d501fe51c4d35
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-07-22 10:27:52 +0700

    Add more externals

--- next.config.js
+++ next.config.js
@@ -2,7 +2,22 @@ module.exports = {
   // target: 'serverless',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
-    config.externals.push('mongodb', 'hdb-pool', 'mysql', 'mysql2')
+    config.externals.push(
+      'mongodb',
+      'hdb-pool',
+      'mysql',
+      'mysql2',
+      'mssql',
+      'oracledb',
+      'pg',
+      'pg-native',
+      'pg-query-stream',
+      'react-native-sqlite-storage',
+      'redis',
+      'sqlite3',
+      'sql.js',
+      'typeorm-aurora-data-api-driver'
+    )
     return config
   },
 }
