5738a5ad241944d5e0a006db29d09e81d44e9592
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-07-22 10:25:32 +0700

    Add mysql2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // target: 'serverless',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
-    config.externals.push('mongodb', 'hdb-pool', 'mysql')
+    config.externals.push('mongodb', 'hdb-pool', 'mysql', 'mysql2')
     return config
   },
 }
