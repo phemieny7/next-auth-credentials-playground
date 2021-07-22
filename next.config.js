9b638bbb8be20b70a3f8559b62679e25d008b8e7
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-07-22 10:22:09 +0700

    Add mysql to externals

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // target: 'serverless',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
-    config.externals.push('mongodb', 'hdb-pool')
+    config.externals.push('mongodb', 'hdb-pool', 'mysql')
     return config
   },
 }
