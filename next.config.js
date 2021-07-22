867c09d8d07ac72a6ca07c2a7d3206ac1388ab90
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-07-22 10:33:26 +0700

    better-sqlite3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // target: 'serverless',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
+    // This should not be necessary in Next Auth v4
     config.externals.push(
       'mongodb',
       'hdb-pool',
@@ -17,7 +18,8 @@ module.exports = {
       'sqlite3',
       'sql.js',
       'typeorm-aurora-data-api-driver',
-      'ioredis'
+      'ioredis',
+      'better-sqlite3'
     )
     return config
   },
